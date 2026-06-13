06aebeb6d9212f50bd92b8fce98f26876869b2fd
vojo12-sys <hegne@freenet.de>
2026-06-13 21:44:31 +0200

    chore: check_api.py entfernt

--- check_api.py
+++
@@ -1,6 +0,0 @@
-import os, httpx
-key = os.environ.get("FOOTBALL_API_KEY", "NICHT GESETZT")
-print("Key:", key[:8] + "..." if len(key) > 8 else key)
-r = httpx.get("https://api.football-data.org/v4/competitions/2000/matches", headers={"X-Auth-Token": key}, timeout=15)
-print("Status:", r.status_code)
-print(r.text[:500])
